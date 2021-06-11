b0d44b9289aa4c238d9cc7e80d491b1c38a39dcd
StevenLignereux <steven.lignereux@hotmail.fr>
2021-06-11 12:15:33 +0200

    affichage commentaires

---
+++ app/Http/Controllers/Front/CommentController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers\Front;
+
+use App\Http\Controllers\Controller;
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class CommentController extends Controller
+{
+    public function __construct()
+    {
+        if(!app()->runningInConsole() && !request()->ajax()) {
+            abort(403);
+        }
+    }
+
+    public function store()
+    {
+
+    }
+
+    public function destroy()
+    {
+
+    }
+
+    public function comments(Post $post)
+    {
+        $comments = $post->validComments()
+                        ->withDepth()
+                        ->latest()
+                        ->get()
+                        ->toTree();
+
+        return [
+            'html' => view('front/comments', compact('comments'))->render(),
+        ];
+    }
+
+}
