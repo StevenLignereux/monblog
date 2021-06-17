dcfb642cdd901b4d0dd57dad6893b6ffbeaa1081
StevenLignereux <steven.lignereux@hotmail.fr>
2021-06-17 22:59:41 +0200

    admin new article feature

--- app/Http/Controllers/Back/PostController.php
+++ app/Http/Controllers/Back/PostController.php
@@ -3,7 +3,14 @@
 namespace App\Http\Controllers\Back;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Back\PostRequest;
+use App\Models\Category;
 use App\Models\Post;
+use App\Repositories\PostRepository;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use App\DataTables\PostsDataTable;
 use Illuminate\Http\Response;
@@ -23,23 +30,42 @@ class PostController extends Controller
 
     /**
      * Show the form for creating a new resource.
-     *
-     * @return Response
+     * @param Integer|null $id
+     * @return Application|Factory|View
      */
-    public function create()
+    public function create(int $id = null)
     {
-        //
+        $post = null;
+
+        if ($id) {
+            $post = Post::findOrFail($id);
+
+            if ($post->user_id === auth()->id()) {
+                $post->title .= ' (2)';
+                $post->slug .= '-2';
+                $post->active = false;
+            } else {
+                $post = null;
+            }
+        }
+
+        $categories = Category::all()->pluck('title', 'id');
+
+        return view('back.posts.form', compact('post', 'categories'));
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param Request $request
-     * @return Response
+     * @param PostRequest $request
+     * @param PostRepository $repository
+     * @return RedirectResponse
      */
-    public function store(Request $request)
+    public function store(PostRequest $request, PostRepository $repository)
     {
-        //
+        $repository->store($request);
+
+        return back()->with('ok', __('The post has been successfully created'));
     }
 
     /**
--- app/Repositories/PostRepository.php
+++ app/Repositories/PostRepository.php
@@ -1,32 +1,38 @@
 <?php
 
 namespace App\Repositories;
-use App\Models\Post;
+
+use App\Http\Requests\Back\PostRequest;
+use Illuminate\Contracts\Pagination\LengthAwarePaginator;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Collection;
+use App\Models\ { Post, Tag };
+use Illuminate\Support\Str;
 
 class PostRepository
 {
     /**
      * Create a query for Post.
      *
-     * @return \Illuminate\Database\Eloquent\Builder
+     * @return Builder
      */
     protected function queryActive()
     {
         return Post::select(
-                      'id',
-                      'slug',
-                      'image',
-                      'title',
-                      'excerpt',
-                      'user_id')
-                    ->with('user:id,name')
-                    ->whereActive(true);
+            'id',
+            'slug',
+            'image',
+            'title',
+            'excerpt',
+            'user_id')
+            ->with('user:id,name')
+            ->whereActive(true);
     }
 
     /**
      * Create a query for Post.
      *
-     * @return \Illuminate\Database\Eloquent\Builder
+     * @return Builder
      */
     protected function queryActiveOrderByDate()
     {
@@ -37,7 +43,7 @@ class PostRepository
      * Get active posts collection paginated.
      *
      * @param  int  $nbrPages
-     * @return \Illuminate\Contracts\Pagination\LengthAwarePaginator
+     * @return LengthAwarePaginator
      */
     public function getActiveOrderByDate($nbrPages)
     {
@@ -47,8 +53,7 @@ class PostRepository
     /**
      * Get heros.
      *
-     * @param  int  $nbrPages
-     * @return \Illuminate\Contracts\Pagination\LengthAwarePaginator
+     * @return Collection|Builder[]
      */
     public function getHeros()
     {
@@ -65,13 +70,13 @@ class PostRepository
     {
         // Post for slug with user, tags and categories
         $post = Post::with(
-                    'user:id,name,email',
-                    'tags:id,tag,slug',
-                    'categories:title,slug'
-                )
-                ->withCount('validComments')
-                ->whereSlug($slug)
-                ->firstOrFail();
+            'user:id,name,email',
+            'tags:id,tag,slug',
+            'categories:title,slug'
+        )
+            ->withCount('validComments')
+            ->whereSlug($slug)
+            ->firstOrFail();
 
         // Previous post
         $post->previous = $this->getPreviousPost($post->id);
@@ -86,28 +91,28 @@ class PostRepository
      * Get previous post
      *
      * @param  integer  $id
-     * @return \Illuminate\Database\Eloquent\Collection
+     * @return Collection
      */
     protected function getPreviousPost($id)
     {
         return Post::select('title', 'slug')
-                    ->whereActive(true)
-                    ->latest('id')
-                    ->firstWhere('id', '<', $id);
+            ->whereActive(true)
+            ->latest('id')
+            ->firstWhere('id', '<', $id);
     }
 
     /**
      * Get next post
      *
      * @param  integer  $id
-     * @return \Illuminate\Database\Eloquent\Collection
+     * @return Collection
      */
     protected function getNextPost($id)
     {
         return Post::select('title', 'slug')
-                    ->whereActive(true)
-                    ->oldest('id')
-                    ->firstWhere('id', '>', $id);
+            ->whereActive(true)
+            ->oldest('id')
+            ->firstWhere('id', '>', $id);
     }
 
     /**
@@ -115,14 +120,14 @@ class PostRepository
      *
      * @param  int  $nbrPages
      * @param  string  $category_slug
-     * @return \Illuminate\Contracts\Pagination\LengthAwarePaginator
+     * @return LengthAwarePaginator
      */
     public function getActiveOrderByDateForCategory($nbrPages, $category_slug)
     {
         return $this->queryActiveOrderByDate()
-                    ->whereHas('categories', function ($q) use ($category_slug) {
-                        $q->where('categories.slug', $category_slug);
-                    })->paginate($nbrPages);
+            ->whereHas('categories', function ($q) use ($category_slug) {
+                $q->where('categories.slug', $category_slug);
+            })->paginate($nbrPages);
     }
 
     /**
@@ -130,29 +135,29 @@ class PostRepository
      *
      * @param  int  $nbrPages
      * @param  integer  $user_id
-     * @return \Illuminate\Contracts\Pagination\LengthAwarePaginator
+     * @return LengthAwarePaginator
      */
     public function getActiveOrderByDateForUser($nbrPages, $user_id)
     {
         return $this->queryActiveOrderByDate()
-                    ->whereHas('user', function ($q) use ($user_id) {
-                        $q->where('users.id', $user_id);
-                    })->paginate($nbrPages);
+            ->whereHas('user', function ($q) use ($user_id) {
+                $q->where('users.id', $user_id);
+            })->paginate($nbrPages);
     }
 
     /**
      * Get active posts for specified tag.
      *
-     * @param  int  $nbrPages
-     * @param  int  $tag_id
-     * @return \Illuminate\Contracts\Pagination\LengthAwarePaginator
+     * @param int $nbrPages
+     * @param $tag_slug
+     * @return LengthAwarePaginator
      */
     public function getActiveOrderByDateForTag($nbrPages, $tag_slug)
     {
         return $this->queryActiveOrderByDate()
-                    ->whereHas('tags', function ($q) use ($tag_slug) {
-                        $q->where('tags.slug', $tag_slug);
-                    })->paginate($nbrPages);
+            ->whereHas('tags', function ($q) use ($tag_slug) {
+                $q->where('tags.slug', $tag_slug);
+            })->paginate($nbrPages);
     }
 
     /**
@@ -160,15 +165,62 @@ class PostRepository
      *
      * @param  int  $n
      * @param  string  $search
-     * @return \Illuminate\Contracts\Pagination\LengthAwarePaginator
+     * @return LengthAwarePaginator
      */
     public function search($n, $search)
     {
         return $this->queryActiveOrderByDate()
-                    ->where(function ($q) use ($search) {
-                        $q->where('excerpt', 'like', "%$search%")
-                          ->orWhere('body', 'like', "%$search%")
-                          ->orWhere('title', 'like', "%$search%");
-                    })->paginate($n);
+            ->where(function ($q) use ($search) {
+                $q->where('excerpt', 'like', "%$search%")
+                    ->orWhere('body', 'like', "%$search%")
+                    ->orWhere('title', 'like', "%$search%");
+            })->paginate($n);
+    }
+
+    /**
+     * Store post.
+     *
+     * @param  PostRequest  $request
+     * @return void
+     */
+    public function store($request)
+    {
+        $request->merge([
+            'active' => $request->has('active'),
+            'image' => basename($request->image),
+        ]);
+
+        $post = $request->user()->posts()->create($request->all());
+
+        $this->saveCategoriesAndTags($post, $request);
+    }
+
+    /**
+     * Save categories and tags.
+     *
+     * @param Post $post
+     * @param  PostRequest  $request
+     * @return void
+     */
+    protected function saveCategoriesAndTags($post, $request)
+    {
+        // Categorie
+        $post->categories()->sync($request->categories);
+
+        // Tags
+        $tags_id = [];
+
+        if($request->tags) {
+            $tags = explode(',', $request->tags);
+            foreach ($tags as $tag) {
+                $tag_ref = Tag::firstOrCreate([
+                    'tag' => ucfirst($tag),
+                    'slug' => Str::slug($tag),
+                ]);
+                $tags_id[] = $tag_ref->id;
+            }
+        }
+
+        $post->tags()->sync($tags_id);
     }
 }
