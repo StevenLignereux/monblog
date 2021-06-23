13ebf0a89248abd3c7092732dd7bc3930980f6d5
StevenLignereux <steven.lignereux@hotmail.fr>
2021-06-23 22:01:34 +0200

    contact feature and social link feature

--- routes/web.php
+++ routes/web.php
@@ -81,5 +81,12 @@ Route::prefix('admin')->group(function () {
         // Users
         Route::resource('users', BackUserController::class)->except(['show', 'create', 'store']);
         Route::name('users.indexnew')->get('newusers', [BackResourceController::class, 'index']);
+
+        // Contacts
+        Route::resource('contacts', BackResourceController::class)->only(['index', 'destroy']);
+        Route::name('contacts.indexnew')->get('newcontacts', [BackResourceController::class, 'index']);
+
+        // Follows
+        Route::resource('follows', BackResourceController::class)->except(['show']);
     });
 });
